e099688016a88e96ed7c92804a4caf91ce3c9b71
kohei <koheitech001@gmail.com>
2023-02-22 22:07:10 +0900

    update test program for cicd

--- test_server.py
+++ test_server.py
@@ -19,18 +19,56 @@ def test_read_main():
     assert type(res.json()) == dict
 
 
-def test_coco():
+def test_image():
+    params0  = dict(
+        test = 1,
+        th_conf = 0.12,
+        th_nms = 0.3
+    )
     with open(f"{path_data}/{name_image}", "rb") as _file:
-        res = testclient.post("/coco_image?test=1", files={"file": (f"_{name_image}", _file, "image/jpeg")})
+        res = testclient.post(
+            "/coco_image", 
+            files={"file": (f"_{name_image}", _file, "image/jpeg")},
+            params=params0
+        )
     assert res.status_code == 200
     assert type(res.json()) == dict
 
-    with open(f"{path_data}/{name_video}", "rb") as _file:
-        res = testclient.post("/coco_video/?test=1", files={"file": (f"_{name_video}", _file, "video/mp4")})
+    params1  = dict(
+        test = 1,
+        filter_categories = [1, 2]
+    )
+    with open(f"{path_data}/{name_image}", "rb") as _file:
+        res = testclient.post(
+            "/coco_image", 
+            params=params1,
+            files={"file": (f"_{name_image}", _file, "image/jpeg")}
+        )
     assert res.status_code == 200
     assert type(res.json()) == dict
+
+def test_video():
+    params2  = dict(
+        test = 1,
+        filter_categories = [1, 2],
+        th_conf = 0.12,
+        th_nms = 0.3
+    )
+    with open(f"{path_data}/{name_video}", "rb") as _file:
+        res = testclient.post(
+            "/coco_video/?test=1", 
+            params=params2,
+            files={"file": (f"_{name_video}", _file, "video/mp4")}
+        )
+    assert res.status_code == 200
+    assert type(res.json()) == dict
+
+    
 
 if __name__ == "__main__":
     
     test_read_main()
-    test_coco()
+    
+    test_image()
+    
+    test_video()
